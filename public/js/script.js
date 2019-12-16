306ef7a3deffad18a1ed19771a5dd9b9055c994c
Anirudh Thakur <anirudh.thakur94@gmail.com>
2019-12-16 22:15:56 +0530

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,15 @@
+//  window.onload = function(){
+
+//       alert("Hello");
+
+//  }
+//alert("hello");
+
+function bookFinder()
+{
+    //document.getElementById("result").innerHTML = "Result will be displayed here";
+    var userInput = document.getElementById("userInput").value; 
+  //  alert(userInput);
+    document.getElementById("result").innerHTML = userInput;
+    return false;
+}
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const exphbr = require('express-handlebars');
+
+// initialise
+const app = express();
+
+
+//create app engine 
+app.engine('handlebars',exphbr(
+    {
+        defaultLayout: 'main'
+    }
+));
+app.set('view engine', 'handlebars');
+
+
+//port
+const port = 3000 || process.env.port;
+
+//express static folder
+app.use(express.static('public'));
+
+
+//handler
+app.get('/',(req,res) => 
+{
+  //  res.send('Welcome to book finder');
+  res.render('home.handlebars'); // res.render('home.handlebars'); change extension when rendering more files
+});
+
+app.get('/about',(req,res) =>
+{
+  res.render('about');
+}
+);
+
+app.get('/contact',(req,res) =>
+{
+  res.render('contact');
+}
+);
+
+//listen to port
+app.listen(3000, () =>
+{
+ // console.log('Server is running on port ${port}');
+  
+  console.log(`Server is running on port ${port}`);
+});
