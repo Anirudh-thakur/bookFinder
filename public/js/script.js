ac3b3ee5fe5f01cdf9f1fe496d7bebb2ddf4a635
Anirudh Thakur <anirudh.thakur94@gmail.com>
2019-12-20 15:19:18 +0530

    Styling & adding view info

--- public/js/script.js
+++ public/js/script.js
@@ -1,55 +1,66 @@
-//  window.onload = function(){
-
-//       alert("Hello");
-
-//  }
-//alert("hello");
-
 function bookFinder()
 {
     //document.getElementById("result").innerHTML = "Result will be displayed here";
     var userInput = document.getElementById("userInput").value; 
   //  alert(userInput);
-   var bookResult = document.getElementById("result") //userInput;
+   var bookResult = document.getElementById("result") 
+
+    //remove existing content on page
+  //  if(bookResult.hasChildNodes)
+  //     bookResult.removeChild();
+
+   //AJAX request to book API
    $.ajax({
      type: "GET",
      url: " https://www.googleapis.com/books/v1/volumes?q="+userInput,
      dataType: "JSON",
      success: function(book)
      {
-       console.log(book);
+       console.log(book); // to get the book JSON in console
+
+       // adding multiple book divs on home page
        for(var i=0;i<book.items.length;i++)
        {
+
          //div for media class
          WrapperDiv = document.createElement("div");
          WrapperDiv.className = "media";
          WrapperDiv.style.marginTop = "50px";
          WrapperDiv.title = "Click to know more";
+
          // creating image
         var image = document.createElement('img');
         image.src = book.items[i].volumeInfo.imageLinks.thumbnail;
         image.className = "mr-3";
+
         //creating div for header
         var Div = document.createElement("div");
         Div.className = "media-body";
+
         //book title
         var head = document.createElement("h3");
         head.innerHTML = book.items[i].volumeInfo.title;
         head.className = "mt-0";
+
         //book author 
-        var author = document.createElement("h4");
+        var author = document.createElement("h5");
         author.innerHTML = "Authors:";
         for(var j=0;j<book.items[i].volumeInfo.authors.length;j++)
         {
-          author.innerHTML = author.innerHTML+" "+book.items[i].volumeInfo.authors[j];
+          author.innerHTML = author.innerHTML+" "+book.items[i].volumeInfo.authors[j]+",";
         }
+
+        //published date
+        var publishDate = document.createElement("p");
+        publishDate.innerHTML = "Published Date: "+book.items[i].volumeInfo.publishedDate;
+
         //Page Count 
-        var pageCount = document.createElement("h5");
-        pageCount.innerHTML = "Pages :"+book.items[i].volumeInfo.pageCount;
+        // var pageCount = document.createElement("p");
+        // pageCount.innerHTML = "Pages :"+book.items[i].volumeInfo.pageCount;
+
         //create description div
         var desc = document.createElement("span");
          var descVar = i;
-                 //alert(descVar);
          desc.setAttribute("id",descVar);
          desc.className = "popuptext";
          var descWrapper = document.createElement("div");
@@ -57,11 +68,26 @@ function bookFinder()
          descWrapper.appendChild(desc);
          //desc.setAttribute("id","Desc"+i);
          //alert(document.getElementById("Desc"+i));
-         //showing description on click
+
+
+         //create preview link 
+         var link = document.createElement("a");
+         link.href = book.items[i].volumeInfo.previewLink;
+         link.target="_blank";
+         link.className = "text-success nounderline";
+         link.innerHTML ="VIEW INFO";
+
+         //wrap preview link in button
+         var LinkButton = document.createElement("button");
+         LinkButton.className = "btn btn-dark";
+         LinkButton.appendChild(link);
+
         //adding to DOM
         Div.appendChild(head);
         Div.appendChild(author);
-        Div.appendChild(pageCount);
+        Div.appendChild(publishDate);
+        //Div.appendChild(pageCount);
+        Div.appendChild(LinkButton);
         Div.appendChild(descWrapper);
         WrapperDiv.appendChild(image);
         WrapperDiv.appendChild(Div);
@@ -70,11 +96,12 @@ function bookFinder()
         WrapperDiv.appendChild(line);
         bookResult.appendChild(WrapperDiv);
 
+        //creating onclick for pop up description
         WrapperDiv.onclick = function()
         {
-          console.log(this.children[1].children[3].children[0]);
-          this.children[1].children[3].children[0].innerHTML = book.items[this.children[1].children[3].children[0].id].volumeInfo.description;
-          this.children[1].children[3].children[0].classList.toggle("show");
+          //console.log(this.children[1].children[5].children[0]);
+          this.children[1].children[4].children[0].innerHTML = book.items[this.children[1].children[4].children[0].id].volumeInfo.description;
+          this.children[1].children[4].children[0].classList.toggle("show");
         };
 
         //bookResult.appendChild(line);
